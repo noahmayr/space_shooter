cf5d1cb17376f438217ab5cb9038575afd9c90b0
Noah <apps@noahmayr.com>
2020-12-09 01:04:54 +0100

    Modified object loading to support directly inherting a NodePath

--- space_shooter/util.py
+++ space_shooter/util.py
@@ -1,32 +1,37 @@
 from direct.gui.OnscreenText import OnscreenText
-from panda3d.core import LPoint2, TextNode, TransparencyAttrib
+from panda3d.core import LPoint2, TextNode, TransparencyAttrib, NodePath
 
 from space_shooter.constants import *
 
 
-def load_object(tex=None, pos=LPoint2(0, 0), depth=SPRITE_POS, scale=1,
-                transparency=True, parent=None):
-    obj = loader.loadModel("assets/models/plane")
-    obj.reparentTo(parent or camera.parent)
+def load_object(*args, **kwargs):
+    return SelfLoadingNodePath(*args, **kwargs)
 
-    obj.setPos(pos.getX(), depth, pos.getY())
-    obj.setScale(0.015 * scale)
 
-    obj.setBin("unsorted", 0)
-    obj.setDepthTest(False)
+class SelfLoadingNodePath(NodePath):
 
-    if transparency:
-        obj.setTransparency(TransparencyAttrib.MAlpha)
+    def __init__(self, tex=None, pos=LPoint2(0, 0), depth=SPRITE_POS, scale=1,
+                 transparency=True, parent=None, model="assets/models/plane"):
+        node = loader.loadModelNode(model)
+        super().__init__(node)
+        self.reparentTo(parent or camera.parent)
 
-    if tex:
-        tex = loader.loadTexture(tex)
-        vec = obj.getScale()
-        vec.x *= tex.getOrigFileXSize()
-        vec.z *= tex.getOrigFileYSize()
-        obj.setScale(vec)
-        obj.setTexture(tex, 1)
+        self.setPos(pos.getX(), depth, pos.getY())
+        self.setScale(0.015 * scale)
 
-    return obj
+        self.setBin("fixed", 0)
+        self.setDepthTest(False)
+
+        if transparency:
+            self.setTransparency(TransparencyAttrib.MAlpha)
+
+        if tex:
+            tex = loader.loadTexture(tex)
+            vec = self.getScale()
+            vec.x *= tex.getOrigFileXSize()
+            vec.z *= tex.getOrigFileYSize()
+            self.setScale(vec)
+            self.setTexture(tex, 1)
 
 
 def gen_label_text(text, i):
